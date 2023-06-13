136f3c44956126cb93f978ae60646a84313d66ad
ExciteNG <65714464+dolaposokoya@users.noreply.github.com>
2023-06-13 14:42:54 +0100

    Added clear interval to stop countdown

--- script.js
+++ script.js
@@ -1,36 +1,6 @@
 AOS.init();
-const reunionDate = new Date('Jun 14,  2023 12:00:00').getTime()
+const reunionDate = new Date('Oct 14,  2023 12:00:00').getTime()
 
-
-// const setReunionDate = () => {
-//     const now = new Date().getTime()
-//     const gap = reunionDate - now
-//     const seconds = 1000
-//     const minutes = seconds * 60
-//     const hours = minutes * 60
-//     const days = hours * 24
-
-//     console.log('Gapo', gap)
-
-//     let day = Math.floor(gap / (days))
-//     let hour = Math.floor((gap % (days)) / hours)
-//     let minute = Math.floor((gap % (hours)) / minutes)
-//     let second = Math.floor((gap % (minutes)) / seconds)
-
-//     day = day >= 10 ? day : `0${day}`
-//     hour = hour >= 10 ? hour : `0${hour}`
-//     minute = minute >= 10 ? minute : `0${minute}`
-//     second = second >= 10 ? second : `0${second}`
-
-//     // $('#').text()
-//     $('#days').text(day)
-//     $('#hours').text(hour)
-//     $('#minutes').html(minute)
-//     $('#seconds').html(second)
-
-// }
-// setReunionDate()
-// setInterval(() => setReunionDate(), 1000)
 
 const countdown = setInterval(function () {
 
