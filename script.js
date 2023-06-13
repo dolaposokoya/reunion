091d6caa3f9da2a8e6d34467b980b7f2b35e8f31
ExciteNG <65714464+dolaposokoya@users.noreply.github.com>
2023-06-13 14:36:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,48 @@
 AOS.init();
-const reunionDate = new Date('Oct 29,  2021 12:00:00').getTime()
+const reunionDate = new Date('Jun 14,  2023 12:00:00').getTime()
 
 
-const setReunionDate = () => {
+// const setReunionDate = () => {
+//     const now = new Date().getTime()
+//     const gap = reunionDate - now
+//     const seconds = 1000
+//     const minutes = seconds * 60
+//     const hours = minutes * 60
+//     const days = hours * 24
+
+//     console.log('Gapo', gap)
+
+//     let day = Math.floor(gap / (days))
+//     let hour = Math.floor((gap % (days)) / hours)
+//     let minute = Math.floor((gap % (hours)) / minutes)
+//     let second = Math.floor((gap % (minutes)) / seconds)
+
+//     day = day >= 10 ? day : `0${day}`
+//     hour = hour >= 10 ? hour : `0${hour}`
+//     minute = minute >= 10 ? minute : `0${minute}`
+//     second = second >= 10 ? second : `0${second}`
+
+//     // $('#').text()
+//     $('#days').text(day)
+//     $('#hours').text(hour)
+//     $('#minutes').html(minute)
+//     $('#seconds').html(second)
+
+// }
+// setReunionDate()
+// setInterval(() => setReunionDate(), 1000)
+
+const countdown = setInterval(function () {
+
+    // Get today's date and time
     const now = new Date().getTime()
     const gap = reunionDate - now
     const seconds = 1000
     const minutes = seconds * 60
     const hours = minutes * 60
     const days = hours * 24
+
+    console.log('Gapo', gap)
 
     let day = Math.floor(gap / (days))
     let hour = Math.floor((gap % (days)) / hours)
@@ -25,6 +59,13 @@ const setReunionDate = () => {
     $('#hours').text(hour)
     $('#minutes').html(minute)
     $('#seconds').html(second)
-}
-setReunionDate()
-setInterval(() => setReunionDate(), 1000)
+
+    // If the count down is over, write some text 
+    if (gap < 0) {
+        clearInterval(countdown);
+        $('#days').text(0)
+        $('#hours').text(0)
+        $('#minutes').html(0)
+        $('#seconds').html(0)
+    }
+}, 1000);
